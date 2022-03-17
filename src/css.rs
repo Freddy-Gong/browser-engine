c7bdc8353497633473c0ab0fcbe549058992467d
Freddy-Gong <21914103@zju.edu.cn>
2022-03-17 16:11:32 +0800

    css初始化

---
+++ src/css.rs
@@ -0,0 +1,84 @@
+use std::default::Default;
+use std::fmt;
+
+pub struct Stylesheet {
+    pub rules: Vec<Rules>,
+}
+
+pub struct Rule {
+    pub selectors: Vec<Selector>,
+    pub declarations: Vec<Declaration>,
+}
+
+pub struct Selector {
+    pub simple: Vec<SimpleSelector>,
+    pub combinators: Vec<char>,
+}
+
+pub struct SimpleSelector {
+    pub tag_name: Option<String>,
+    pub id: Option<String>,
+    pub classes: Vec<String>,
+}
+
+pub struct Declaration {
+    pub property: String,
+    pub value: Value,
+}
+
+pub enum Value {
+    Color(Color),
+    Length(f32, Unit),
+    Other(String),
+}
+
+pub enum Unit {
+    Em,
+    Ex,
+    Ch,
+    Rem,
+    Vh,
+    Vw,
+    Vmain,
+    Vmax,
+    Px,
+    Mm,
+    Q,
+    Cm,
+    In,
+    Pt,
+    Pc,
+    Pct,
+}
+
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
+}
+
+impl Stylesheet {
+    pub fn new(rules: Vec<Rule>) -> Stylesheet {
+        Stylesheet { rules }
+    }
+}
+
+impl Default for Stylesheet {
+    fn default() -> Self {
+        Stylesheet { rules: Vec::new() }
+    }
+}
+
+impl fmt::Debug for Stylesheet {
+    fn fmt(&self,f:&mut fmt:Formatter) ->fmt::Result {
+        let mut rule_result = String::new();
+        for rule in &self.rules {
+            if rule_result.len() > 0 {
+                rule_result.push_str("\n\n");
+            }
+            rule_result.push_str(&format!("{:?}",rule))
+        }
+        write!(f,"{}",rule_result)
+    }
+}
